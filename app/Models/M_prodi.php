1c2bbcef3f3ffcfc5de2d31831db6820fbea8dd0
wahyuos <wahyukamaludin@outlook.com>
2021-08-11 22:13:14 +0700

    tambah method overview prodi

--- app/Models/M_prodi.php
+++ app/Models/M_prodi.php
@@ -8,4 +8,11 @@ class M_prodi extends Model
 {
     protected $table = "ref_prodi";
     protected $primaryKey = 'kode_prodi';
+
+    public function overviewProdi($kode_prodi, $id_smt)
+    {
+        return $this->db->table('v_overview_prodi')
+            ->where(['kode_prodi' => $kode_prodi, 'id_smt' => $id_smt])
+            ->get()->getRowArray();
+    }
 }
