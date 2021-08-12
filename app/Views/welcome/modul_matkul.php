8a44fff37feb6e0f4a77594fd9cdaa2403a37827
mucisdev <mucisdev@gmail.com>
2021-08-12 21:19:14 +0700

    tambah kolom overview mata kuliah

--- app/Views/welcome/modul_matkul.php
+++ app/Views/welcome/modul_matkul.php
@@ -22,7 +22,33 @@
 </div>
 
 <div class="row">
-    <div class="col-12 col-lg-6">
+    <div class="col-12">
+        <div class="card" style="background-image: url('<?= base_url('assets/img/bg-profil.jpg') ?>');background-size:cover;min-height:100px;">
+            <div class="card-body py-4">
+                <p class="card-text text-light">Mata Kuliah</p>
+                <h2 class="text-white font-weight-bold d-flex align-items-center">
+                    <?= strtoupper($overview['nm_mk']); ?>
+                </h2>
+                <p class="card-text text-light mb-5">PROGRAM STUDI <?= strtoupper($overview['jenjang'] . ' ' . $overview['nm_prodi']); ?></p>
+                <div class="d-flex justify-content-start overflow-auto">
+                    <div class="me-5">
+                        <p class="card-text text-light mb-0">Total materi</p>
+                        <h4 class="text-light"><?= $overview['jml_materi'] ?></h4>
+                    </div>
+                    <div>
+                        <p class="card-text text-light mb-0">Total tugas</p>
+                        <h4 class="text-light"><?= $overview['jml_tugas'] ?></h4>
+                    </div>
+                </div>
+                <hr>
+                <a class="btn btn-light" onclick="history.go(-1)"><i class="align-middle fas fa-fw fa-arrow-left"></i> KEMBALI</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-12 ">
         <div class="card">
             <div class="card-header">
                 <h5 class="card-title">Materi</h5>
@@ -32,23 +58,6 @@
                 <ol class="list-group list-group-flush" id="load-materi">
 
                 </ol>
-            </div>
-        </div>
-    </div>
-    <div class="col-12 col-lg-6">
-        <div class="card">
-            <div class="card-header">
-                <h5 class="card-title">Tugas</h5>
-                <h6 class="card-subtitle text-muted">Tugas yang tersedia</h6>
-            </div>
-            <div class="card-body">
-
-                <div id="tasks-progress">
-                    <ol class="list-group list-group-flush" id="load-tugas">
-
-                    </ol>
-                </div>
-
             </div>
         </div>
     </div>
