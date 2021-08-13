21c33d93ef549970222cebc599c869fc2cea405d
Mucis Developer <83754433+mucisdev@users.noreply.github.com>
2021-08-13 22:28:58 +0700

    tambah method baru dan perbaikan

--- app/Controllers/Welcome.php
+++ app/Controllers/Welcome.php
@@ -33,68 +33,71 @@ class Welcome extends BaseController
 	public function kelas($kode_prodi = null)
 	{
 		$data = [
-			'title' => 'Daftar Kelas',
-			'semester_aktif' => $this->semester_aktif->id_smt,
-			'overview' => $this->prodi->overviewProdi($kode_prodi, $this->semester_aktif->id_smt), // info detail dari prodi yang dipilih
+			'title' => 'Kelas',
+			'kode_prodi' => $kode_prodi
 		];
 		return view('welcome/kelas_prodi', $data);
 	}
 
 	public function matkul($id_kls = null, $smt = null)
 	{
-		// ambil data mata kuliah berdasarkan kelas, semester, dan id_smt
-		$data_matkul = $this->matkul->dataMatkulKelas($id_kls, $smt, $this->semester_aktif->id_smt);
-		// pengecekan data maktul
-		if ($data_matkul) :
-			$data = [
-				'title' => 'Daftar Mata Kuliah',
-				'data_matkul' => $data_matkul,
-				'overview' => $this->kelas->overviewKelas($id_kls), // info detail dari kelas yang dipilih
-				'mahasiswa' => $this->mahasiswa->dataMahasiswaKelas($id_kls), // daftar mahasiswa pada kelas tersebut
-			];
-			return view('welcome/matkul_prodi', $data);
-		else :
-			return $this->not_found('Mata kuliah tidak ditemukan.');
-		endif;
+		$data = [
+			'title' => 'Mata Kuliah',
+			'id_kls' => $id_kls,
+			'smt' => $smt,
+		];
+		return view('welcome/matkul_prodi', $data);
 	}
 
 	public function modul($id_kls = null, $id_mk = null)
 	{
-		$data_matkul = $this->matkul->overviewMatkul($id_mk, $id_kls);
-		// pengecekan
-		if ($data_matkul) :
-			// jika data ada, ambil data berdasarkan id_mk dan id_kls
-			$overview = $data_matkul;
-		else :
-			// jika data ada, ambil data HANYA berdasarkan id_mk
-			$overview = $this->matkul->overviewMatkul($id_mk);
-		endif;
 		$data = [
-			'title' => 'Daftar Modul Mata Kuliah',
-			'overview' => $overview, // info detail dari matkul yang dipilih
+			'title' => 'Materi',
+			'id_kls' => $id_kls,
+			'id_mk' => $id_mk,
 		];
 		return view('welcome/modul_matkul', $data);
 	}
 
-	private function not_found($message)
+	public function get_prodi()
 	{
-		$data = [
-			'title' => 'Halaman Tidak Ditemukan',
-			'message' => $message
-		];
-		return view('welcome/not-found', $data);
+		// cek post data
+		if ($this->request->getVar()) {
+			// ambil data prodi
+			$prodi = $this->prodi->findAll();
+			// pengecekan prodi
+			if ($prodi) :
+				$response = response(true, 'Data ditemukan!');
+				$response['field'] = $prodi; // data prodi
+			else :
+				$response = response(false, 'Tidak ada Program Studi!');
+			endif;
+		} else {
+			$response = response(false, 'Tidak ada data yang diterima!');
+		}
+
+		// kirim nilai csrf yang terbaru
+		$response['csrf_token'] = csrf_hash();
+		// return $response dalam bentuk json
+		return $this->response->setJSON($response);
 	}
 
 	public function get_kelas()
 	{
+		// cek post data
 		if ($this->request->getVar()) {
+			// kode_prodi
 			$kode_prodi = $this->request->getVar('kode_prodi');
+			// ambil kelas berdasarkan prodi yg dipilih
 			$kelas = $this->kelas->dataKelasProdi($kode_prodi, $this->semester_aktif->id_smt);
+			// pengecekan ketersediaan kelas
 			if ($kelas) :
+				// response yang dikirim
 				$response = response(true, 'Data ditemukan!');
-				$response['field'] = $kelas;
+				$response['overview_prodi'] = $this->prodi->overviewProdi($kode_prodi, $this->semester_aktif->id_smt); // data overview
+				$response['field'] = $kelas; // data kelas
 			else :
-				$response = response(false, 'Tidak ada data yang diterima!');
+				$response = response(false, 'Program studi yang dipilih tidak memiliki kelas.');
 			endif;
 		} else {
 			$response = response(false, 'Tidak ada data yang diterima!');
@@ -108,35 +111,23 @@ class Welcome extends BaseController
 
 	public function get_matkul()
 	{
+		// cek post data
 		if ($this->request->getVar()) {
+			// kode_prodi
 			$id_kls = $this->request->getVar('id_kls');
+			// semester
 			$smt = $this->request->getVar('smt');
+			// ambil matkul berdasarkan kelas yang dipilih
 			$matkul = $this->matkul->dataMatkulKelas($id_kls, $smt, $this->semester_aktif->id_smt);
+			// pengecekan ketersediaan matkul
 			if ($matkul) :
+				// respons yang dikirim
 				$response = response(true, 'Data ditemukan!');
-				$response['field'] = $matkul;
+				$response['overview_kelas'] = $this->kelas->overviewKelas($id_kls);
+				$response['mahasiswa'] = $this->mahasiswa->dataMahasiswaKelas($id_kls);
+				$response['field'] = $matkul; // data matkul
 			else :
-				$response = response(false, 'Tidak ada data yang diterima!');
-			endif;
-		} else {
-			$response = response(false, 'Tidak ada data yang diterima!');
-		}
-
-		// kirim nilai csrf yang terbaru
-		$response['csrf_token'] = csrf_hash();
-		// return $response dalam bentuk json
-		return $this->response->setJSON($response);
-	}
-
-	public function get_prodi()
-	{
-		if ($this->request->getVar()) {
-			$prodi = $this->prodi->findAll();
-			if ($prodi) :
-				$response = response(true, 'Data ditemukan!');
-				$response['field'] = $prodi;
-			else :
-				$response = response(false, 'Tidak ada data yang diterima!');
+				$response = response(false, 'Tidak ada mata kuliah pada kelas ini!');
 			endif;
 		} else {
 			$response = response(false, 'Tidak ada data yang diterima!');
@@ -150,37 +141,21 @@ class Welcome extends BaseController
 
 	public function get_materi()
 	{
+		// cek post data
 		if ($this->request->getVar()) {
+			// id_kls kelas
 			$id_kls = $this->request->getVar('id_kls');
+			// id_mk matakuliah
 			$id_mk = $this->request->getVar('id_mk');
+			// ambil materi berdasarkan kelas dan matkul
 			$list_materi = $this->matkul->listMateriMatkul($id_kls, $id_mk);
+			// pengecekan ketersediaan materi
 			if ($list_materi) :
 				$response = response(true, 'Data ditemukan!');
-				$response['field'] = $list_materi;
+				$response['overview_matkul'] = $this->matkul->overviewMatkul($id_mk);
+				$response['field'] = $list_materi; // data materi
 			else :
-				$response = response(false, 'Tidak ada data yang diterima!');
-			endif;
-		} else {
-			$response = response(false, 'Tidak ada data yang diterima!');
-		}
-
-		// kirim nilai csrf yang terbaru
-		$response['csrf_token'] = csrf_hash();
-		// return $response dalam bentuk json
-		return $this->response->setJSON($response);
-	}
-
-	public function get_tugas()
-	{
-		if ($this->request->getVar()) {
-			$id_kls = $this->request->getVar('id_kls');
-			$id_mk = $this->request->getVar('id_mk');
-			$list_tugas = $this->matkul->listTugasMatkul($id_kls, $id_mk);
-			if ($list_tugas) :
-				$response = response(true, 'Data ditemukan!');
-				$response['field'] = $list_tugas;
-			else :
-				$response = response(false, 'Tidak ada data yang diterima!');
+				$response = response(false, 'Mata kuliah ini belum memiliki materi apapun.');
 			endif;
 		} else {
 			$response = response(false, 'Tidak ada data yang diterima!');
