168510432152373d182fdfa360243e77951f78db
Mucis Developer <83754433+mucisdev@users.noreply.github.com>
2021-08-21 09:15:21 +0700

    perubahan halaman login

--- app/Views/auth/index.php
+++ app/Views/auth/index.php
@@ -3,44 +3,39 @@
 
 <div class="row m-0 p-0">
     <div class="col-sm-8 col-md-6 col-lg-4 mx-auto p-0 pt-md-4">
-        <div class="card shadow-none">
-            <div class="card-body">
-                <div class="m-0 m-md-4">
-                    <div class="text-center mb-4">
-                        <div class="text-center mb-3">
-                            <img src="/assets/img/icon.png" alt="Logo" width="40%" height="auto" class="img-fluid rounded-circle">
-                        </div>
-                        <h1 class="h2">Halaman Login</h1>
-                        <p>
-                            Silahkan login untuk masuk sistem
-                        </p>
-                    </div>
-                    <hr>
-                    <form autocomplete="off" class="formInput">
-                        <?= csrf_field('csrf_token'); ?>
-                        <div class="mb-3">
-                            <input class="form-control required" type="username" name="username" id="username" placeholder="Enter your username" autofocus required />
-                        </div>
-                        <div class="mb-3">
-                            <input class="form-control required" type="password" name="password" id="my_password" placeholder="Enter your password" required />
-                        </div>
-                        <div>
-                            <div class="form-check align-items-center">
-                                <input class="form-check-input" type="checkbox" value="" id="show-password" onclick="show_hide_pass()">
-                                <label class="form-check-label" for="show-password">
-                                    Lihat password
-                                </label>
-                            </div>
-                        </div>
-                        <div class="text-center mt-4">
-                            <div class="d-grid gap-2">
-                                <button class="btn btn-lg btn-primary btn-block btnSubmit">MASUK</button>
-                                <button class="btnReset d-none"></button>
-                            </div>
-                        </div>
-                    </form>
+        <div class="m-0 m-md-4">
+            <div class="text-center mb-4">
+                <div class="text-center mb-4">
+                    <img src="/assets/img/icon.png" alt="Logo" width="30%" height="auto" class="img-fluid rounded-circle">
                 </div>
+                <h1 class="h2">Halaman Login</h1>
+                <p>
+                    Silahkan login untuk masuk sistem
+                </p>
             </div>
+            <form autocomplete="off" class="formInput">
+                <?= csrf_field('csrf_token'); ?>
+                <div class="mb-3">
+                    <input class="form-control required" type="username" name="username" id="username" placeholder="Username" autofocus required />
+                </div>
+                <div class="mb-3">
+                    <input class="form-control required" type="password" name="password" id="my_password" placeholder="Password" required />
+                </div>
+                <div>
+                    <div class="form-check align-items-center">
+                        <input class="form-check-input" type="checkbox" value="" id="show-password" onclick="show_hide_pass()">
+                        <label class="form-check-label" for="show-password">
+                            Lihat password
+                        </label>
+                    </div>
+                </div>
+                <div class="text-center mt-4">
+                    <div class="d-grid gap-2">
+                        <button class="btn btn-lg btn-primary btn-block btnSubmit">MASUK</button>
+                        <button class="btnReset d-none"></button>
+                    </div>
+                </div>
+            </form>
         </div>
 
         <div class="col-12 text-center">
